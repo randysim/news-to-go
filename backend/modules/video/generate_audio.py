c1fbe992ca7f30663185fb35acc69676349af8f9
Randy <randysim9@gmail.com>
2025-03-17 20:49:05 -0400

    feat: yapper voice

--- backend/modules/video/generate_audio.py
+++ backend/modules/video/generate_audio.py
@@ -3,7 +3,7 @@ from yapper import Yapper, PiperSpeaker, PiperVoiceUK
 
 vanilla_yapper = Yapper()
 speaker = PiperSpeaker(
-    voice=PiperVoiceUK.ALAN
+    voice=PiperVoiceUK.SOUTHERN_ENGLISH_FEMALE
 )
 
 
@@ -27,24 +27,7 @@ def generate_audio_from_script(script, name, directory):
     body = get_within_tags("BODY", script)
     outro = get_within_tags("OUTRO", script)
 
-    sentences = hook.split(".") + body.split(".") + outro.split(".")
-    audio_files = []
-
-    sub_directory = f"{directory}/{name}"
-    i = 0
-    while os.path.exists(sub_directory):
-        i += 1
-        sub_directory = f"{directory}/{name}_{i}"
-    os.mkdir(sub_directory)
-
-    for sentence in sentences:
-        if not sentence:
-            continue
-
-        sentence = sentence.strip()
-        audio_files.append((sentence, generate_audio_file(sentence, f"{name}_{len(audio_files)}", sub_directory)))
-
-    return audio_files
+    return generate_audio_file(f"{hook} {body} {outro}", name, directory)
 
 if __name__ == "__main__":
     TEST_SCRIPT = """<HOOK>Tumult in Myanmar's political landscape as the military government pushes for national elections amid growing pressure from domestic and foreign influences.</HOOK>
@@ -55,5 +38,5 @@ Civilians increasingly demand democratic accountability amid ongoing civil war. 
 The People's Defence Force (PDF) has targeted civilians involved in election preparations, arresting teachers and others in southern regions, creating a climate of fear. Some observers, like Zaw Kyaw from the National Union of Goldberg and Assistance League, believe the military will eventually collapse due to low morale regardless of election outcomes.</BODY>
 <OUTRO>Who will be next in line to lead Myanmar? Will the junta hold until the end, or will democracy prevail? The fate of this small nation, and its people, hangs in the balance. What do you think? Lets discuss in the comments below.</OUTRO>"""
 
-    audio_files = generate_audio_file(TEST_SCRIPT, "test_script_53242", "audio")
+    audio_files = generate_audio_from_script(TEST_SCRIPT, "test_script_53242", "audio")
     
